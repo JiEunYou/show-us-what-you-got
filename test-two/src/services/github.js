8dfcb2177644080749014e9e89ed24617c4fa335
Ji <ji@jicello.com>
2017-02-01 22:38:35 +1100

    Added getRepositoriesForUser

--- test-two/src/services/github.js
+++ test-two/src/services/github.js
@@ -10,7 +10,15 @@ class GitHubService {
     }
 
     getUsersForOrganisation(organisationId) {
+        // GET /orgs/:org/members
+
         return this.http.get(this.baseUrl + "orgs/" + organisationId + "/members" + this.authenticationQueryString);
+    }
+
+    getRepositoriesForUser(userId) {
+        // GET /users/:username/repos
+
+        return this.http.get(this.baseUrl + "users/" + userId + "/repos" + this.authenticationQueryString);
     }
 }
 
